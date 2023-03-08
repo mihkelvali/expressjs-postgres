15cb6d854a418849ccdb8cd7267995003c7ebcf7
Mihkel Väli <mihkel@bobw.co>
2023-03-08 19:27:40 +0200

    db test

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import pg from "pg";
 
 // Connect to the database using the DATABASE_URL environment
 //   variable injected by Railway
-// const pool = new pg.Pool();
+const pool = new pg.Pool();
 
 const app = express();
 const port = process.env.PORT || 3333;
@@ -13,8 +13,9 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  // const { rows } = await pool.query("SELECT NOW()");
-  res.send(JSON.stringify({ text: "todo1" }));
+  const { rows } = await pool.query("SELECT * FROM todos");
+  console.log(rows);
+  res.send(JSON.stringify(rows));
 });
 
 app.listen(port, () => {
